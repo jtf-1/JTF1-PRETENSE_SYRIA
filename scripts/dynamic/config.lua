65e0d796ebed968218305aa40abfc283a104d66f
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-09-11 21:41:28 +0100

    Create config.lua

---
+++ scripts/dynamic/config.lua
@@ -0,0 +1,7 @@
+Config = Config or {}
+Config.lossCompensation = 1.0 -- gives advantage to the side with less zones. Set to 0 to disable
+Config.randomBoost = 0.0004 -- adds a random factor to build speeds that changes every 30 minutes, set to 0 to disable
+Config.buildSpeed = 10 -- structure and defense build speed (smaller number longer build times)
+Config.supplyBuildSpeed = 85 -- supply helicopters and convoys build speed (smaller number longer build times)
+Config.missionBuildSpeedReduction = 0.096 -- reduction of build speed in case of ai missions (smaller number longer build times)
+Config.maxDistFromFront = Config.maxDistFromFront or 129640 -- max distance in meters from front after which zone is forced into low activity state (export mode)
