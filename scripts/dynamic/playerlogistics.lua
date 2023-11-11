70cf4f610c825e57f03c5bbdc4bc2fd814d87701
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-11-11 14:38:04 +0000

    Update playerlogistics.lua

--- scripts/dynamic/playerlogistics.lua
+++ scripts/dynamic/playerlogistics.lua
@@ -2,4 +2,5 @@ PlayerLogistics.allowedTypes['AH-64D_BLK_II'] = { supplies = true, personCapacit
 PlayerLogistics.allowedTypes['Ka-50'] = { supplies = true, personCapacity = 4}
 PlayerLogistics.allowedTypes['SA342L'] = { supplies = true, personCapacity = 4}
 PlayerLogistics.allowedTypes['SA342M'] = { supplies = true, personCapacity = 4}
-PlayerLogistics.allowedTypes['SA342Minigun'] = { supplies = true, personCapacity = 4}PlayerLogistics.allowedTypes['Ka-50_3'] = { supplies = true, personCapacity = 4}
+PlayerLogistics.allowedTypes['SA342Minigun'] = { supplies = true, personCapacity = 4}
+PlayerLogistics.allowedTypes['Ka-50_3'] = { supplies = true, personCapacity = 4}
